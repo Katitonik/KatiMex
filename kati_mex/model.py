702af29bb8d756f4c2c74d6e10110d22d11be245
axlER8R <axl.mattheus@gmail.com>
2020-05-08 08:47:56 +1200

    Refactor model
    * Fix error
    * Optimise code layout

--- kati_mex/model.py
+++ kati_mex/model.py
@@ -65,7 +65,7 @@ class OrderHeader(NamedTuple):
 def new_order_header(
     taker: Employee,
     deliver: bool,
-    address: None
+    address: List[str]
 ) -> OrderHeader:
     result = OrderHeader(
         date=datetime.now(), taker=taker, deliver=deliver, address=address
@@ -89,41 +89,33 @@ class OrderItem(NamedTuple):
     item_price: Decimal
 
 
-DISH_PRICES = {
-    'Nachos': Decimal('10.00'),
-    'Hard shell tacos': Decimal('12.00'),
-    'Soft shell tacos': Decimal('11.50'),
-    'Chiles Rellenos': Decimal('12.50'),
-    'Chimichangas': Decimal('12.00'),
-    'Chilato de Pollo': Decimal('14.00'),
-    'Sopa de Cameron': Decimal('13.50'),
-    'Cochnita pibli': Decimal('11.00'),
-    'Mole': Decimal('10.00'),
-    'Aguachile': Decimal('15.00'),
-    'Enchiladas': Decimal('9.00'),
-}
-
-SIDE_PRICES = {
-    'Salsa': Decimal('0.50'),
-    'Guakamole': Decimal('4.50'),
-    'Sour Cream': Decimal('3.50'),
-    'Chips': Decimal('3.00'),
-    'Soft Drink': Decimal('2.50'),
-}
-
-
-def _lookup_price(
-    item: Union[Dish, Side],
-    size: Size
-) -> Decimal:
-    price_list = DISH_PRICES if type(item) is Dish else SIDE_PRICES
+def _lookup_price(item: Union[Dish, Side], size: Size) -> Decimal:
+    price_list = {
+        # Dishes
+        'Nachos': Decimal('10.00'),
+        'Hard shell tacos': Decimal('12.00'),
+        'Soft shell tacos': Decimal('11.50'),
+        'Chiles Rellenos': Decimal('12.50'),
+        'Chimichangas': Decimal('12.00'),
+        'Chilato de Pollo': Decimal('14.00'),
+        'Sopa de Cameron': Decimal('13.50'),
+        'Cochnita pibli': Decimal('11.00'),
+        'Mole': Decimal('10.00'),
+        'Aguachile': Decimal('15.00'),
+        'Enchiladas': Decimal('9.00'),
+        # Sides
+        'Guakamole': Decimal('4.50'),
+        'Sour Cream': Decimal('3.50'),
+        'Chips': Decimal('3.00'),
+        'Soft Drink': Decimal('2.50')
+    }
     scale_factor = {
         Size.Small: Decimal('1.00'),
         Size.Medium: Decimal('1.20'),
         Size.Large: Decimal('1.30')
     }
-    price = price_list.get(item.name)
-    scale = scale_factor.get(size)
+    price = price_list.get(item.name, Decimal(0))
+    scale = scale_factor.get(size, Decimal(0))
     result = price * scale
     return result
 
@@ -154,7 +146,7 @@ class OrderFooter(NamedTuple):
 def new_order_footer(items: List[OrderItem]) -> OrderFooter:
     sub_total = Decimal('0.00')
     for item in items:
-        sub_total += item.price
+        sub_total += item.item_price
     tax = sub_total * Decimal('0.14')
     total = sub_total + tax
 
