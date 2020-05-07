27a5c52e48a9561725011513e1dd855f5432a17e
axlER8R <axl.mattheus@gmail.com>
2020-05-08 08:49:15 +1200

    Apply minor changes to form

--- kati_mex/view/order_form.py
+++ kati_mex/view/order_form.py
@@ -1,14 +1,9 @@
-import Tkinter as Tk
-import Tix
+import tkinter as Tk
+import sqlite3 as SQL
 
-root = Tix.Tk()
 
-order_list = Tix.HList(root, columns = 5, header = True)
-order_list.header_create(0, text = "File")
-order_list.header_create(1, text = "Date")
-order_list.header_create(2, text = "Size")
-order_list.add_row("row0", text = "filename.txt")
-order_list.item_create(entry_path, 1, text = "2009-03-26 21:07:03")
-order_list.item_create(entry_path, 2, text = "200MiB")
+order_form = Tk()
+SQL.connect('')
 
-app = Tk()
+
+order_form.mainloop()
