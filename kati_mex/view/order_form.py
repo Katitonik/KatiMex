0e96322fa58f1bb526dee636132a57ac0a9d4dac
axlER8R <axl.mattheus@gmail.com>
2020-05-19 20:04:38 +1200

    Update order form

--- kati_mex/view/order_form.py
+++ kati_mex/view/order_form.py
@@ -2,72 +2,69 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 
-def new_order():
-    pass
-
-
-def clear_order():
-    pass
-
-
-def clear_order_header():
-    pass
-
-
-def clear_order_items():
-    pass
-
-
-def clear_order_footer():
-    pass
-
-
-def _get_order_header():
-    pass
-
-
-def _get_order_items():
-    pass
-
-
-def _get_order_footer():
-    pass
-
-
-def save_order():
-    pass
-
-
+# form
 form = tk.Tk()
+
+# header variables
+var_cust = tk.StringVar()
+var_contact = tk.StringVar()
+var_deliver = tk.BooleanVar()
+var_street = tk.StringVar()
+var_suburb = tk.StringVar()
+
+# item variables
+var_item = tk.StringVar()
+var_spic = tk.StringVar()
+var_size = tk.StringVar()
+var_qty = tk.IntVar()
+
+
+# item events
+def add_item():
+    pass
+
+
+def remove_item():
+    pass
+
+
+# order events
+def cancel_order():
+    pass
+
+
+def ok_order():
+    pass
+
+
+def exit_order():
+    import sys
+    sys.exit()
+
 
 # header
 lbl_header = tk.Label(master=form, text='Customer Details')
 frm_header = tk.Frame(master=form, relief=tk.RAISED, borderwidth=1)
 
-var_cust = tk.StringVar()
 lbl_cust = tk.Label(master=frm_header, text='Customer:')
 ent_cust = tk.Entry(master=frm_header, textvariable=var_cust)
 
-var_contact = tk.StringVar()
 lbl_contact = tk.Label(master=frm_header, text='Contact:')
 ent_contact = tk.Entry(master=frm_header, textvariable=var_contact)
 
-var_deliver = tk.BooleanVar()
 rbtn_collect = tk.Radiobutton(master=frm_header, text='Collect',
                               variable=var_deliver, value=False)
 rbtn_deliver = tk.Radiobutton(master=frm_header, text='Deliver',
                               variable=var_deliver, value=True)
 
-var_street = tk.StringVar()
 lbl_street = tk.Label(master=frm_header, text='Street:')
 ent_street = tk.Entry(master=frm_header, textvariable=var_street)
 
-var_suburb = tk.StringVar()
 lbl_suburb = tk.Label(master=frm_header, text='Suburb:')
 cbx_suburb = ttk.Combobox(master=frm_header, textvariable=var_suburb)
 
-lbl_header.grid(row=0, column=0)
-frm_header.grid(row=1, column=0)
+lbl_header.grid(row=0, column=0, sticky=tk.W)
+frm_header.grid(row=1, column=1)
 lbl_cust.grid(row=1, column=0)
 ent_cust.grid(row=1, column=1)
 lbl_contact.grid(row=1, column=2)
@@ -81,37 +78,36 @@ cbx_suburb.grid(row=3, column=3)
 
 # items
 lbl_items = tk.Label(master=form, text='Items')
-frm_items = tk.Frame(master=form, relief=tk.GROOVE)
+frm_items = tk.Frame(master=form, relief=tk.RAISED)
 
 lbl_items.grid(row=5, column=0)
 frm_items.grid(row=6, column=0)
 
-var_item = tk.StringVar()
 lbl_item = tk.Label(master=frm_items, text='Item')
 ent_item = ttk.Combobox(master=frm_items, textvar=var_item)
 
-var_spic = tk.StringVar()
 lbl_spic = tk.Label(master=frm_items, text='Spicyness:')
 cbx_spic = ttk.Combobox(master=frm_items, textvariable=var_spic)
 
-var_qty = tk.IntVar()
 lbl_qty = tk.Label(master=frm_items, text='Quantity:')
 ent_qty = tk.Entry(master=frm_items, textvariable=var_qty)
 
-btn_add = tk.Button(master=frm_items, text='+')
-btn_remove = tk.Button(master=frm_items, text='-')
+btn_add = tk.Button(master=frm_items, command=add_item, text='+')
+btn_remove = tk.Button(master=frm_items, command=remove_item, text='-')
 
 lst_items = tk.Listbox(master=frm_items, width=96, selectmode=tk.SINGLE)
 
-lbl_item.grid(row=7, column=0)
-ent_item.grid(row=7, column=1)
-lbl_spic.grid(row=7, column=2)
-cbx_spic.grid(row=7, column=3)
-lbl_qty.grid(row=7, column=4)
-ent_qty.grid(row=7, column=5)
-btn_add.grid(row=7, column=6)
-btn_remove.grid(row=8, column=6, sticky=tk.N)
-lst_items.grid(row=8, columnspan=6)
+lbl_items.grid(row=4, column=0, sticky=tk.W)
+frm_items.grid(row=5, column=1)
+lbl_item.grid(row=6, column=0)
+ent_item.grid(row=6, column=1)
+lbl_spic.grid(row=6, column=2)
+cbx_spic.grid(row=6, column=3)
+lbl_qty.grid(row=6, column=4)
+ent_qty.grid(row=6, column=5)
+btn_add.grid(row=6, column=6)
+btn_remove.grid(row=7, column=6, sticky=tk.N)
+lst_items.grid(row=7, columnspan=6)
 
 for i in "1,2,3,4".split(','):
     lst_items.insert(tk.END, i)
@@ -124,18 +120,21 @@ lbl_tax = tk.Label(master=frm_footer, text='Tax:')
 lbl_del = tk.Label(master=frm_footer, text='Delivery Charge:')
 lbl_ttl = tk.Label(master=frm_footer, text='Total:')
 
-lbl_footer.grid(row=9, column=0)
-frm_footer.grid(row=10, column=0)
-lbl_sub.grid(row=11, column=6, sticky=tk.E)
-lbl_tax.grid(row=12, column=6, sticky=tk.E)
-lbl_del.grid(row=13, column=6, sticky=tk.E)
-lbl_ttl.grid(row=14, column=6, sticky=tk.E)
+lbl_footer.grid(row=8, column=0, sticky=tk.W)
+frm_footer.grid(row=9, column=1)
+lbl_sub.grid(row=10, column=6, sticky=tk.E)
+lbl_tax.grid(row=11, column=6, sticky=tk.E)
+lbl_del.grid(row=12, column=6, sticky=tk.E)
+lbl_ttl.grid(row=13, column=6, sticky=tk.E)
 
 # buttons
-btn_ok = tk.Button(master=form, width=10, text='Ok')
-btn_cancel = tk.Button(master=form, width=10, text='Cancel')
-btn_ok.grid(row=15, column=2)
-btn_cancel.grid(row=15, column=3)
+btn_ok = tk.Button(master=form, command=ok_order, width=10, text='Ok')
+btn_cancel = tk.Button(master=form, command=cancel_order, width=10, text='Cancel')
+btn_exit = tk.Button(master=form, command=exit_order, width=10, text='Exit')
+
+btn_ok.grid(row=12, column=2)
+btn_cancel.grid(row=13, column=2)
+btn_exit.grid(row=14, column=2)
 
 
 form.mainloop()
